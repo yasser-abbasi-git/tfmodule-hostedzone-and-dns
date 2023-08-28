b834aa0d44a62b8c3e87524be1de7e63088d8424
yasser_abbasi <yasser_abbasi@yahoo.com>
2023-08-28 14:16:40 +0100

    fix(infrastructure): Add type and description to variables

--- variables.tf
+++ variables.tf
@@ -1,8 +1,31 @@
-variable "root_domain" {}
-variable "hosted_zone_domain" {}
-variable "hosted_zone_name" {}
-variable "subdomain" {}
-variable "a_record_alias_domain_name" {}
-variable "a_record_alias_zone_id" {}
+variable "root_domain" {
+    description = "Root domain to use for dns delegation"
+    type        = string
+}
+
+variable "hosted_zone_domain" {
+    description = "Domain for the hosted zone to create"
+    type        = string
+}
+
+variable "hosted_zone_name" {
+    description = "Domain name for the hosted zone to create NS record for in the root domain"
+    type        = string
+}
+
+variable "subdomain" {
+    description = "Subdomain to create A record for"
+    type        = string
+}
+
+variable "a_record_alias_domain_name" {
+    description = "Domain name of the resource to point the A record to"
+    type        = string
+}
+
+variable "a_record_alias_zone_id" {
+    description = "Hosted zone id of the resource to point the A record to"
+    type        = string
+}
 
 
